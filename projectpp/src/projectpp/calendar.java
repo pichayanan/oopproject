538dbc86f5b86f1113743f096b101cb9acc78a56
DELL <DELL@172.19.26.79>
2018-04-20 19:44:53 +0700

    pro v.1

--- projectpp/src/projectpp/calendar.java
+++ projectpp/src/projectpp/calendar.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projectpp;
 
-/**
- *
- * @author PalmFat
- */
 import java.util.*;
   
 public class calendar {
    public static void day(){
       try { 
-         System.out.println("      "+new Date( )); 
-          
-        } catch (Exception e) { 
-          System.out.println("Got an exception!"); 
-      }
+         System.out.println("                  "+new Date( )); 
+        } 
+      catch (Exception e) { 
+         System.out.println("Got an exception!"); 
+        }
    }
 }
